61b64730143e3f02545b51b9122f93712e30bba6
Dion Cheng <dunz.zheng@gmail.com>
2023-02-19 22:50:15 +0800

    Product function - Query product list

--- src/main/java/com/iancheng/springbootmall/dao/ProductDao.java
+++ src/main/java/com/iancheng/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.iancheng.springbootmall.dao;
 import com.iancheng.springbootmall.dto.ProductRequest;
 import com.iancheng.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
